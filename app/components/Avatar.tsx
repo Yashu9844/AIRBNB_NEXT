fee50154c92bf92e39002d873461bf534d47c48c
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-08-29 00:14:09 +0530

    add Nav Bar to airnbnb clone

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+  <Image
+   src="/images/Avatar.jpeg"
+   height={"20"}
+   width={"20"}
+   alt="Avatar"
+   
+  
+  />
+  );
+};
+
+export default Avatar;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="text-red-500">
+  <div className="text-red-500 ">
     Hello Airbnb!
   </div>
   );
