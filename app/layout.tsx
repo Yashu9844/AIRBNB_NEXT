f956b54247d55d4d7f178ac5419162c137c1db9a
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-08-31 21:58:49 +0530

    complete Model dialog box

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Modal />
+<Modal isOpen={true} actionLabel="Submit" title="Hello world" />
         <Nav/>
         {children}</body>
     </html>
