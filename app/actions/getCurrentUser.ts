f15c7cf6c6f9b956a869c04f228c5fd841e1f563
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-09-01 16:07:30 +0530

    add currentuser get it from getServverSesssion

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,1 +1,32 @@
+import { getServerSession, } from "next-auth/next";
+import authOptions from "../libs/authOptions";
+import prisma from '@/app/libs/prismadb'
 
+
+export async function getSession(){
+    return await getServerSession(authOptions)
+}
+
+export default async function getCurrentUser(){
+    try {
+        const session:any = await getSession();
+        if (!(session as any)?.user?.email) {
+            return null;
+        }
+const currentuser = await prisma.user.findUnique({
+    where:{
+        email:session.user.email as string,
+    }
+})
+if(!currentuser){
+    return null; 
+}
+
+return currentuser;
+
+        
+    } catch (error:any) {
+        return null;
+        
+    }
+}
