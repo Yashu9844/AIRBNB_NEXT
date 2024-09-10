61e926a0c668a1c7a85be87c9dfedd55bc58222a
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-09-11 00:36:20 +0530

    add delete function for listing and fav

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request,{params}:{params:IParams}){
  const {listingId} = params;
 
  if(!listingId || typeof listingId !== "string"){
-    throw new Error("listingId must be a string or Not Found Lissting Id");
+    throw new Error("listingId must be a string or Not Found Listing Id");
  }
 let favoriteIds = [...(currentUser.favoriteIds || [])]
 
@@ -36,4 +36,33 @@ return NextResponse.json(user)
 
 }
 
+
+export async function DELETE(request:Request,{params}:{params:IParams}){
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser){
+        return NextResponse.error();
+    }
+ const {listingId} = params;
+
+if(!listingId || typeof listingId !== 'string'){
+    throw new Error("listingId must be a string or Not Found Listing Id");
+}
+
+let favoriteIds = [...(currentUser.favoriteIds || [])]
+
+favoriteIds = favoriteIds.filter((id)=>id!== listingId);
+
+const user = await prisma.user.update({
+where:{
+    id:currentUser.id
+},
+data:{
+    favoriteIds:favoriteIds,
+}
+
+})
+
+return NextResponse.json(user)
+}
 
