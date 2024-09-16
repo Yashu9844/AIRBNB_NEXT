282f13525edc15f000d081ff9fbd65f4c91ea4b4
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-09-16 14:28:13 +0530

    bug fix 05

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,10 +1,14 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-declare global{
-    var prisma: PrismaClient | undefined
+declare global {
+  // Allows the global prisma instance to be recognized in TypeScript
+  var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient()
-if(process.env.NODE_ENV !== 'production')globalThis.prisma = client
+const client = globalThis.prisma || new PrismaClient();
 
-export default client
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = client;  // Store the Prisma client globally in development
+}
+
+export default client;  // Export the singleton Prisma client
